e1af82065d8e47e21e8b397db16cc81940bfec17
nawa <nawa@sra.co.jp>
2026-01-27 15:56:35 +0900

    ・ヘッダーを切り出したため、各Formからヘッダーの記載を削除 ・CalcFormのメインコンテナの高さをヘッダーの高さを引いたものに調整

--- src/client/src/components/pages/CALC/form/CalcForm/CalcForm.tsx
+++ src/client/src/components/pages/CALC/form/CalcForm/CalcForm.tsx
@@ -6,8 +6,7 @@ import { ViewIdType } from '@front/stores/TEST/test/testStore/index';
 import { useMemo, useState, useCallback, useEffect } from 'react';
 import { FormProvider, useForm, useFieldArray } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Box, Stack, Paper, Divider, Select, MenuItem } from '@mui/material';
-import SummarizeIcon from '@mui/icons-material/Summarize';
+import { Box, Stack, Divider, Select, MenuItem } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import ImportButton from '@front/components/ui/Button/ImportButton';
@@ -545,16 +544,8 @@ function CalcForm(props: Props) {
     }, [projectType, ipaValueType, setValue]);
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 72px)' }}>
             <FormProvider {...methods}>
-                {/* ヘッダー */}
-                <Paper elevation={0} sx={{ bgcolor: '#1976d2', color: 'white', p: 2, borderRadius: 0 }}>
-                    <FlexBox gap={1}>
-                        <SummarizeIcon sx={{ fontSize: 32 }} />
-                        <Text variant="pageTitle">見積作成支援ツール</Text>
-                    </FlexBox>
-                </Paper>
-
                 {/* メインコンテンツエリア */}
                 <Box sx={{ display: 'flex', flex: 1, overflow: 'hidden' }}>
                     {/* 左サイドバー - 案件情報 */}
