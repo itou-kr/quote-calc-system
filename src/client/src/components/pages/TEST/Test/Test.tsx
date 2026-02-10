87106ff18f7426e163ac04e545529b6102e3e131
nagisa <nagisa@sra.co.jp>
2026-02-10 10:50:57 +0900

    レイアウトをCalcに合わせる

--- src/client/src/components/pages/TEST/Test/Test.tsx
+++ src/client/src/components/pages/TEST/Test/Test.tsx
@@ -7,48 +7,49 @@ import { Container } from '@mui/system';
 
 
 const Test = memo (() => {
-    const data = useTypedSelector((state) => state.calc.data)
+    // const data = useTypedSelector((state) => state.calc.data)
     const { isDirty } = useTypedSelector((state) => state.test);
 
-    const processRatios = {
-        basicDesign: data?.processRatios?.basicDesign ?? 0,
-        detailedDesign: data?.processRatios?.detailedDesign ?? 0,
-        implementation: data?.processRatios?.implementation ?? 0,
-        integrationTest: data?.processRatios?.integrationTest ?? 0,
-        systemTest: data?.processRatios?.systemTest ?? 0,
-    };
+    // const processRatios = {
+    //     basicDesign: data?.processRatios?.basicDesign ?? 0,
+    //     detailedDesign: data?.processRatios?.detailedDesign ?? 0,
+    //     implementation: data?.processRatios?.implementation ?? 0,
+    //     integrationTest: data?.processRatios?.integrationTest ?? 0,
+    //     systemTest: data?.processRatios?.systemTest ?? 0,
+    // };
 
-    const processManMonths = {
-        basicDesign: data?.processManMonths?.basicDesign ?? 0,
-        detailedDesign: data?.processManMonths?.detailedDesign ?? 0,
-        implementation: data?.processManMonths?.implementation ?? 0,
-        integrationTest: data?.processManMonths?.integrationTest ?? 0,
-        systemTest: data?.processManMonths?.systemTest ?? 0,
-    };
+    // const processManMonths = {
+    //     basicDesign: data?.processManMonths?.basicDesign ?? 0,
+    //     detailedDesign: data?.processManMonths?.detailedDesign ?? 0,
+    //     implementation: data?.processManMonths?.implementation ?? 0,
+    //     integrationTest: data?.processManMonths?.integrationTest ?? 0,
+    //     systemTest: data?.processManMonths?.systemTest ?? 0,
+    // };
 
-    const processDurations = {
-        basicDesign: data?.processDurations?.basicDesign ?? 0,
-        detailedDesign: data?.processDurations?.detailedDesign ?? 0,
-        implementation: data?.processDurations?.implementation ?? 0,
-        integrationTest: data?.processDurations?.integrationTest ?? 0,
-        systemTest: data?.processDurations?.systemTest ?? 0,
-    }
+    // const processDurations = {
+    //     basicDesign: data?.processDurations?.basicDesign ?? 0,
+    //     detailedDesign: data?.processDurations?.detailedDesign ?? 0,
+    //     implementation: data?.processDurations?.implementation ?? 0,
+    //     integrationTest: data?.processDurations?.integrationTest ?? 0,
+    //     systemTest: data?.processDurations?.systemTest ?? 0,
+    // }
 
     return (
-        <Container disableGutters sx={{ height: '100%' }}>
+        // <Container disableGutters sx={{ height: '100%' }}>
             <TestForm 
                 viewId="TEST" 
                 isDirty={isDirty} 
-                data={{
-                    ...data,                    
-                    projectName: data?.projectName ?? '',
-                    productivityFPPerMonth: data?.productivityFPPerMonth ?? 0,
-                    processRatios: processRatios,
-                    processManMonths: processManMonths,
-                    processDurations: processDurations
-                }}
+                // data={{
+                //     ...data,                    
+                //     projectName: data?.projectName ?? '',
+                //     productivityFPPerMonth: data?.productivityFPPerMonth ?? 0,
+                //     processRatios: processRatios,
+                //     processManMonths: processManMonths,
+                //     processDurations: processDurations
+            
+                // }}
             />
-        </Container>
+        // </Container>
     );
 });
 
