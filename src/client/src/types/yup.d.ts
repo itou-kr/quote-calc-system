f807d8ec80df28d02b93bcd733044104c5190a38
nagisa <nagisa@sra.co.jp>
2026-02-13 14:19:42 +0900

    トランザクションテーブルの必須チェック実装

--- src/client/src/types/yup.d.ts
+++ src/client/src/types/yup.d.ts
@@ -1,10 +1,14 @@
 // import { StringSchema, NumberSchema, ObjectSchema, MixedSchema } from "yup";
 
-import { NumberSchema } from 'yup';
+import { NumberSchema, ObjectSchema } from 'yup';
 
 declare module 'yup' {
     // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
     export interface NumberSchema {
         rangeCheck(minField: number, maxField: number): this;
+    }
+    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+    export interface ObjectSchema {
+        transactionPairCheck(): this;
     }
 }
